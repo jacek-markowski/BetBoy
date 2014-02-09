7387e0350cbd6829d225eacb40b60fa8cc8c2cd7
Jacek Markowski <jacek87markowski@gmail.com>
2014-02-09 19:34:54 +0100

    update web_links.py added new files creation: old, current

--- tools/web_links.py
+++ tools/web_links.py
@@ -4,13 +4,14 @@ import re
 
 with open('links.txt','r') as files:
     links = files.readlines()
-
+current = []
+old = []
 for url in links:
     source = urllib2.urlopen(url).read()
     url_split = url.split('/')
     print url_split[:]
     re_patternA ='<a href="/soccer/%s/(?P<link>.*?/).*?>'%url_split[-2]
-    print re_patternA
+    #print re_patternA
     check = re.compile(re_patternA, re.DOTALL)
     search = check.findall(source)
     with open('betexplorer/%s' %url_split[-2], 'w') as league:
@@ -18,5 +19,16 @@ for url in links:
             path = search[i].split('/')
             name_league = url_split[-2] + '_'+path[-2]
             line = name_league +' '+url[:-1]+search[i]+'\n'
+            
             if (not 'cup' in path[-2]) and (not 'trophy' in path[-2]):
+                if ('20' in path[-2]) or ('19' in path[-2]):
+                    old.append(line)
+                else:
+                    current.append(line)
                 league.write(line)
+    with open('current','w') as cur:
+        for i in current:
+            cur.write(i)
+    with open('old','w') as old_f:
+        for i in old:
+            old_f.write(i)
